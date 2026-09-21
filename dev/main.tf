b60a8af8b82f842c61b2b9f8809e03ba9aba4215
Paul Praveen Kumar Athmuri <praveen.athmuri@gmail.com>
2026-09-21 15:58:46 +0530

    m7

--- dev/main.tf
+++ dev/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "prav-tf-test-buck "
+    bucket = "prav-tf-test-buck"
     key = "dev/terraform.tfstate"
     region = "ap-south-1"
     
